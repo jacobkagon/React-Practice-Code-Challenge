2dde05d105728ab780b077777bdadc43d896ec21
JakesProjects1 <jacob@kagon.net>
2020-12-16 16:07:03 -0500

    done with core deliverables

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -1,20 +1,74 @@
-import React, { Component } from 'react';
-import SushiContainer from './containers/SushiContainer';
-import Table from './containers/Table';
+import React, { Component } from "react";
+import SushiContainer from "./containers/SushiContainer";
+import Table from "./containers/Table";
 
 // Endpoint!
-const API = "http://localhost:3000/sushis"
+const API = "http://localhost:3000/sushis";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      sushi: [],
+      money: 100,
+      plates: [],
+      sushiCount: 0,
+    };
+  }
+
+  onlyFour = () => {
+    return this.state.sushi.slice(this.state.sushiCount, this.state.sushiCount+4);
+  };
+
+  nextFour = () => {
+    const newSushi = this.state.sushiCount + 4
+    this.setState({
+      sushiCount: newSushi
+    })
+  }
+
+  addMoney = (event) => {
+    this.setState({
+      money: this.state.money + event.target.value
+    })
+
+  }
+
+  handleSushiClick = (sushi) => {
+    // sushi image goes away
+    // money decreases by the amount of the sushi clicked
+    // a plate get added to the table
+
+
+    const newMoney = this.state.money - sushi.price 
+    
+    if (!this.state.plates.includes(sushi) && newMoney >= 0) {
+        this.setState({
+        plates: [...this.state.plates, sushi],
+        money: newMoney
+      });
+    }
+  }
+
+  async componentDidMount() {
+    const response = await fetch(API);
+    const json = await response.json();
+    this.setState({ sushi: json });
+  }
 
   render() {
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <SushiContainer sushiData={this.onlyFour()} 
+        nextFour={this.nextFour}
+        handleSushiClick={this.handleSushiClick}
+          plates={this.state.plates}
+        />
+        <Table money={this.state.money} plates={this.state.plates}/>
       </div>
     );
   }
+
 }
 
 export default App;
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,19 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
 const SushiContainer = (props) => {
+ 
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
+      { props.sushiData.map(sushi => {
+        return <Sushi sushi={sushi} 
+        handleClick={props.handleSushiClick} 
+        key={sushi.id} 
+        plates={props.plates}/>
+      })}
+        <MoreButton onlyFour={props.nextFour}/>
       </div>
     </Fragment>
   )
